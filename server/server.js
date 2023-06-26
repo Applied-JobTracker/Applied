691b23473580ee396e3f0ec74b8045857a806252
LeRocque <t.lerocque@gmail.com>
2023-06-26 15:47:57 -0400

    dependencies updated

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const myRouter = require('./routes/myRoute');
 
