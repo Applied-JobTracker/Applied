2722c756a753f18a4dc6ced62e2dec0bd11f5125
Fab3005 <fabrizzio7112@outlook.com>
2023-06-26 19:54:25 -0400

    added the query for editApp

--- server/controllers/appsController.js
+++ server/controllers/appsController.js
@@ -21,6 +21,10 @@ const appsController = {
   },
 
   editApp: (req, res, next) => {
+    console.log('entered editApps in the appController middleware');
+    const { company_name, date, app_form, stack} = req.body;
+    const tableName = 'application';
+    const query = `UPDATE ${tableName} SET company_name = ${company_name}, SET dat = ${date}, SET app_form = ${app_form} SET stack = ${stack}`;
     return next();
   },
 
