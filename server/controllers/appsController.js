518588367886aba207bd7bcf346c4c9ebe2e129d
Fab3005 <fabrizzio7112@outlook.com>
2023-06-26 16:45:23 -0400

    added the try block for the client query

--- server/controllers/appsController.js
+++ server/controllers/appsController.js
@@ -1,9 +1,17 @@
+const { Client } = require('pg');
+const connectionString = process.env.PG_URI;
+const client = new Client({ connectionString });
+
 const appsController = {
-  getApps: (req, res, next) => {
-    const tableName = 'application'
+  getApps: async (req, res, next) => {
+    const tableName = 'application';
     const query = `SELECT * FROM ${tableName}`;
 
-    return next();
+    try {
+      const result = await client.query(query);
+      req.tableData = result.rows;
+      return next();
+    } 
   },
 
   addApp: (req, res, next) => {
