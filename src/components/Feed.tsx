f144ea3ef6eb726d0051f21814fcf126145e9714
James McCollough <100531603+j-mccoll@users.noreply.github.com>
2023-06-27 11:57:19 -0400

    Configure Feed to get and render apps by user

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,58 +1,43 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import FeedItem from './FeedItem';
 import '../css/Feed.css';
 import { UserProps } from '../FrontendTypes';
 
 export default function Feed(props: UserProps) {
-  const feedItems : JSX.Element[] = [];
-  // Send a get request to the server at the '/apps' endpoint to get all applications in the database
-  // NOTE: might want to add logic to limit the number of responses 
-  fetch('/apps')
+  const prevState : JSX.Element[] = [];
+  const [ feedItems, setFeedItems ] = useState(prevState);
+
+  useEffect(() => {
+    // Send a get request to the server at the '/apps/user_id' endpoint to get all applications in the database
+    // NOTE: might want to add logic to limit the number of responses 
+    fetch(`/apps/${props.userId}`)
     // parse response from json to js
     .then(response => response.json())
     // NOTE: confirm with backend how the data is received, but assuming for now the response data is an array of objects
     // iterate through response data, pushing individual FeedItems to the feedItems array with relevant props
     .then(response => {
+      const array : JSX.Element[] = [];
       for (const app of response) {
         // NOTE: confirm how data is received from backend
-        feedItems.push(<FeedItem 
+        array.push(<FeedItem 
           company={app.company_name}
           date={app.date}
           appType={app.app_form}
           stack={app.stack} 
           appID={app.application_id}
+          key={app.application_id}
         />);
       }
+      setFeedItems(array);
     })
     .catch(err => {
       console.log('There was an error fetching applications: ', err);
-  });
+    });
+  }, [ feedItems ]);
 
   return (
     <div className='feed'>
       <h1>My Applications</h1>
-      {/* Delete the 3 manually rendered FeedItems below - temporary here for styling purposes */}
-      <FeedItem 
-        company={'Google'}
-        date={new Date()}
-        appType={'Easy apply'}
-        stack={'fullstack'} 
-        appID={'023779'}
-      />
-      <FeedItem 
-        company={'Google'}
-        date={new Date()}
-        appType={'Easy apply'}
-        stack={'fullstack'} 
-        appID={'023779'}
-      />
-      <FeedItem 
-        company={'Google'}
-        date={new Date()}
-        appType={'Easy apply'}
-        stack={'fullstack'} 
-        appID={'023779'}
-      />
       {feedItems}
     </div>
   );
