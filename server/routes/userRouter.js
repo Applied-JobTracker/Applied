30203bfad225f96fd0b8e887718b2c1b4e95decb
Fab3005 <fabrizzio7112@outlook.com>
2023-06-27 22:29:13 -0400

    converted userControlleer.ts into typescript

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import userController from '../controllers/userController.js';
+import userController from '../controllers/userController.ts';
 
 const userRouter = express.Router();
 
