93308fbde4233dc1f48a5c0bffc0bf8e5ffcd77b
James McCollough <100531603+j-mccoll@users.noreply.github.com>
2023-06-28 01:29:08 -0400

    Set up React Context to manage Feed rerenders

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
+import { Context } from './Context';
 
 function App() {
-
+  const [context, setContext] = useState(false);
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/home' element={<MainPage />}/>
-      </Routes>
+      <Context.Provider value={[context, setContext]}>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/home' element={<MainPage />}/>
+        </Routes>
+      </Context.Provider>
     </div>
   );
 }
